77d854d411d3c8295b09e6224c0502c3e0126800
Carter Chapman <ChapCart405@GoTVDSB.ca>
2022-03-21 11:00:33 -0400

    Cannot Enter Valid Bet After Invalid Has Been Entered

--- Part 5 - Dice Game/Form1.cs
+++ Part 5 - Dice Game/Form1.cs
@@ -16,7 +16,6 @@ namespace Part_5___Dice_Game
         {
             InitializeComponent();
         }
-        //Always gets doubles - Must fix
         Random generator1 = new Random();
         int die1;
         int die2;
@@ -28,141 +27,133 @@ namespace Part_5___Dice_Game
         private void btnRoll_Click(object sender, EventArgs e)
         {
             die1 = generator1.Next(1, 7);
-            switch (die1)
+            if (double.TryParse(txtBetAmount.Text, out betAmount))
             {
-                case 1:
-                    rtbDice.Text = ("  __________\n" +
-                    "  |                    |\n" +
-                    "  |                    |\n" +
-                    "  |          O       |\n" +
-                    "  |                    |\n" +
-                    "  |__________|");
-                    break;
-                case 2:
-                    rtbDice.Text = ("  __________\n" +
-                    "  |                    |\n" +
-                    "  |      O            |\n" +
-                    "  |                    |\n" +
-                    "  |            O     |\n" +
-                    "  |__________|");
-                    break;
-                case 3:
-                    rtbDice.Text = ("  __________\n" +
-                    "  |                    |\n" +
-                    "  |     O            |\n" +
-                    "  |         O        |\n" +
-                    "  |             O    |\n" +
-                    "  |__________|");
-                    break;
-                case 4:
-                    rtbDice.Text = ("  __________\n" +
-                    "  |                   |\n" +
-                    "  |   O       O    |\n" +
-                    "  |                   |\n" +
-                    "  |   O       O    |\n" +
-                    "  |__________|");
-                    break;
-                case 5:
-                    rtbDice.Text = ("  __________\n" +
-                    "  |                   |\n" +
-                    "  |   O       O    |\n" +
-                    "  |        O         |\n" +
-                    "  |   O       O    |\n" +
-                    "  |__________|");
-                    break;
-                default:
-                    rtbDice.Text = ("  __________\n" +
-                    "  |                   |\n" +
-                    "  |   O       O    |\n" +
-                    "  |   O       O    |\n" +
-                    "  |   O       O    |\n" +
-                    "  |__________|");
-                    break;
-            }
-
-            die2 = generator1.Next(1, 7);
-            switch (die2)
-            {
-                case 1:
-                    rtbDice2.Text = ("  __________\n" +
-                    "  |                    |\n" +
-                    "  |                    |\n" +
-                    "  |          O       |\n" +
-                    "  |                    |\n" +
-                    "  |__________|");
-                    break;
-                case 2:
-                    rtbDice2.Text = ("  __________\n" +
-                    "  |                    |\n" +
-                    "  |      O            |\n" +
-                    "  |                    |\n" +
-                    "  |            O     |\n" +
-                    "  |__________|");
-                    break;
-                case 3:
-                    rtbDice2.Text = ("  __________\n" +
-                    "  |                    |\n" +
-                    "  |     O            |\n" +
-                    "  |         O        |\n" +
-                    "  |             O    |\n" +
-                    "  |__________|");
-                    break;
-                case 4:
-                    rtbDice2.Text = ("  __________\n" +
-                    "  |                   |\n" +
-                    "  |   O       O    |\n" +
-                    "  |                   |\n" +
-                    "  |   O       O    |\n" +
-                    "  |__________|");
-                    break;
-                case 5:
-                    rtbDice2.Text = ("  __________\n" +
-                    "  |                   |\n" +
-                    "  |   O       O    |\n" +
-                    "  |        O         |\n" +
-                    "  |   O       O    |\n" +
-                    "  |__________|");
-                    break;
-                default:
-                    rtbDice2.Text = ("  __________\n" +
-                    "  |                   |\n" +
-                    "  |   O       O    |\n" +
-                    "  |   O       O    |\n" +
-                    "  |   O       O    |\n" +
-                    "  |__________|");
-                    break;
-            }
-            //Gambling If Statements
-                if (betAmount <= bankAccount)
+                switch (die1)
                 {
-                    lblError.Text = ("");
-                    if (double.TryParse(txtBetAmount.Text, out betAmount))
+                    case 1:
+                        rtbDice.Text = ("  __________\n" +
+                        "  |                    |\n" +
+                        "  |                    |\n" +
+                        "  |          O       |\n" +
+                        "  |                    |\n" +
+                        "  |__________|");
+                        break;
+                    case 2:
+                        rtbDice.Text = ("  __________\n" +
+                        "  |                    |\n" +
+                        "  |      O            |\n" +
+                        "  |                    |\n" +
+                        "  |            O     |\n" +
+                        "  |__________|");
+                        break;
+                    case 3:
+                        rtbDice.Text = ("  __________\n" +
+                        "  |                    |\n" +
+                        "  |     O            |\n" +
+                        "  |         O        |\n" +
+                        "  |             O    |\n" +
+                        "  |__________|");
+                        break;
+                    case 4:
+                        rtbDice.Text = ("  __________\n" +
+                        "  |                   |\n" +
+                        "  |   O       O    |\n" +
+                        "  |                   |\n" +
+                        "  |   O       O    |\n" +
+                        "  |__________|");
+                        break;
+                    case 5:
+                        rtbDice.Text = ("  __________\n" +
+                        "  |                   |\n" +
+                        "  |   O       O    |\n" +
+                        "  |        O         |\n" +
+                        "  |   O       O    |\n" +
+                        "  |__________|");
+                        break;
+                    default:
+                        rtbDice.Text = ("  __________\n" +
+                        "  |                   |\n" +
+                        "  |   O       O    |\n" +
+                        "  |   O       O    |\n" +
+                        "  |   O       O    |\n" +
+                        "  |__________|");
+                        break;
+                }
+
+                die2 = generator1.Next(1, 7);
+                switch (die2)
+                {
+                    case 1:
+                        rtbDice2.Text = ("  __________\n" +
+                        "  |                    |\n" +
+                        "  |                    |\n" +
+                        "  |          O       |\n" +
+                        "  |                    |\n" +
+                        "  |__________|");
+                        break;
+                    case 2:
+                        rtbDice2.Text = ("  __________\n" +
+                        "  |                    |\n" +
+                        "  |      O            |\n" +
+                        "  |                    |\n" +
+                        "  |            O     |\n" +
+                        "  |__________|");
+                        break;
+                    case 3:
+                        rtbDice2.Text = ("  __________\n" +
+                        "  |                    |\n" +
+                        "  |     O            |\n" +
+                        "  |         O        |\n" +
+                        "  |             O    |\n" +
+                        "  |__________|");
+                        break;
+                    case 4:
+                        rtbDice2.Text = ("  __________\n" +
+                        "  |                   |\n" +
+                        "  |   O       O    |\n" +
+                        "  |                   |\n" +
+                        "  |   O       O    |\n" +
+                        "  |__________|");
+                        break;
+                    case 5:
+                        rtbDice2.Text = ("  __________\n" +
+                        "  |                   |\n" +
+                        "  |   O       O    |\n" +
+                        "  |        O         |\n" +
+                        "  |   O       O    |\n" +
+                        "  |__________|");
+                        break;
+                    default:
+                        rtbDice2.Text = ("  __________\n" +
+                        "  |                   |\n" +
+                        "  |   O       O    |\n" +
+                        "  |   O       O    |\n" +
+                        "  |   O       O    |\n" +
+                        "  |__________|");
+                        break;
+                }
+                if (betAmount <= bankAccount && betAmount > 0)
+                {
+                    if (die1 == die2)
                     {
-                        if (double.TryParse(lblBankAccount.Text, out bankAccount))
-                        {
-                            bankAccountRun = bankAccount - betAmount;
-                            lblBankAccount.Text = Convert.ToString(bankAccountRun);
-                        }
-
-                        if (die1 == die2)
-                        {
-                            payReturn = betAmount * 2;
-                            newAccountTotal = payReturn + bankAccountRun;
-                            lblBankAccount.Text = Convert.ToString(newAccountTotal);
-                        }
-                        else if (die1 != die2)
-                        {
-                            payReturn = betAmount * 1.5;
-                            newAccountTotal = payReturn + bankAccountRun;
-                            lblBankAccount.Text = Convert.ToString(newAccountTotal);
-                        }
+                        payReturn = betAmount * 2;
+                        bankAccount = (bankAccountRun + payReturn);
+                        lblBankAccount.Text = bankAccount + "";
                     }
-
+                    else if (die1 != die2)
+                    {
+                        payReturn = betAmount * 1.5;
+                        bankAccount = (bankAccountRun + payReturn);
+                        lblBankAccount.Text = bankAccount + "";
+                    }
                 }
                 else
                 {
-                    lblError.Text = ("Error! Insufficient Funds!");
+                    lblError.Text = ($"Please enter a valid bet amount. (Between $0 and {bankAccount}");
                 }
+            }
+            
         }
     } 
 }
